fe993e8c100cae1b056bcac324f4800403fc0246
pickier <ito@mba.local>
2013-12-10 18:51:04 +0900

    comment

--- setup.sh
+++ setup.sh
@@ -4,10 +4,9 @@ DOT_FILES=(.zshrc .vimrc .tmux.conf)
 
 for file in ${DOT_FILES[@]}
 do
-    ln -s $HOME/dotfiles/$file $HOME/$file
+  touch $HOME/$file
+  rm $HOME/$file
+  ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
 chmod u+x $HOME/dotfiles/bin/tmuxx.sh
-
-mkdir -p $HOME/.vim/bundle
-git clone http://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
--- setup_osx.sh
+++ setup_osx.sh
@@ -12,3 +12,10 @@ sudo defaults write com.apple.loginwindow LogoutHook /Library/Scripts/mute-on.sh
 sudo defaults write com.apple.loginwindow LoginHook /Library/Scripts/mute-off.sh
 
 chmod u+x $HOME/dotfiles/bin/reattach-to-user-namespace
+
+ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go/install)"
+
+brew install tmux
+
+mkdir -p $HOME/.vim/bundle
+git clone http://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
