dd1a2eb9348a6df7e19af163125c158529b793f1
pickier <ito@mba.local>
2014-08-08 19:51:21 +0900

    comment

--- bin/tmuxx.sh
+++ bin/tmuxx.sh
@@ -3,23 +3,17 @@
 # attach to an existing tmux session, or create one if none exist
 # also set up access to the system clipboard from within tmux when possible
 
-if [[ ( $OSTYPE == darwin* ) && ( -x $(which reattach-to-user-namespace 2>/dev/null) ) ]]; then
-  # on OS X force tmux's default command to spawn a shell in the user's namespace
-  # https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
-  tweaked_config=$(cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
-
-  # 5分割
-  tmux attach || (tmux -f <(echo "$tweaked_config") new-session \; split-window -h \; resize-pane -L 30 \; split-window -d \; resize-pane -D 10 \; split-window -h \; select-pane -D \; split-window -h -d \; resize-pane -R 20 \; select-pane -R \; clock-mode \; select-pane -L \; select-pane -U \; select-pane -L \; select-pane -L)
-
-  # 3分割
-  #tmux attach || (tmux -f <(echo "$tweaked_config") new-session \; split-window -h \; split-window \; clock-mode \; select-pane -U \; select-pane -L)
-
-else
-
-  # 5分割
-  tmux attach || (tmux new-session \; split-window -h \; resize-pane -L 30 \; split-window -d \; resize-pane -D 10 \; split-window -h \; select-pane -D \; split-window -h -d \; resize-pane -R 20 \; select-pane -R \; clock-mode \; select-pane -L \; select-pane -U \; select-pane -L)
-
-  # 3分割
-  #tmux attach || (tmux new-session \; split-window -h \; split-window \; clock-mode \; select-pane -U \; select-pane -L)
-
+if [ -z $TMUX ]; then
+  if $(tmux has-session 2> /dev/null); then
+    tmux attach
+  else
+    if [[ ( $OSTYPE == darwin* ) && ( -x $(which reattach-to-user-namespace 2>/dev/null) ) ]]; then
+      # on OS X force tmux's default command to spawn a shell in the user's namespace
+      # https://github.com/ChrisJohnsen/tmux-MacOSX-pasteboard
+      tweaked_config=$(cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
+      (tmux -f <(echo "$tweaked_config") new-session \; split-window -h \; resize-pane -L 30 \; split-window -d \; resize-pane -D 10 \; split-window -h \; select-pane -D \; split-window -h -d \; resize-pane -R 20 \; select-pane -R \; clock-mode \; select-pane -L \; select-pane -U \; select-pane -L \; select-pane -L)
+    else
+      (tmux new-session \; split-window -h \; resize-pane -L 30 \; split-window -d \; resize-pane -D 10 \; split-window -h \; select-pane -D \; split-window -h -d \; resize-pane -R 20 \; select-pane -R \; clock-mode \; select-pane -L \; select-pane -U \; select-pane -L)
+    fi
+  fi
 fi
--- setup.sh
+++ setup.sh
@@ -9,12 +9,16 @@ do
   ln -s $HOME/dotfiles/$file $HOME/$file
 done
 
-chmod u+x $HOME/dotfiles/bin/getCpuUsage.sh
-chmod u+x $HOME/dotfiles/bin/getMemUsage.sh
-chmod u+x $HOME/dotfiles/bin/tmuxx.sh
+UTIL_FILES=( getCpuUsage.sh getMemUsage.sh tmuxx.sh)
+
+for file in ${UTIL_FILES[@]}
+do
+  chmod u+x $HOME/dotfiles/bin/$file
+done
 
 mkdir -p $HOME/.vim/bundle
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/neobundle.vim
 git clone https://github.com/erikw/tmux-powerline.git ~/.tmux/tmux-powerline
 
-cp ~/dotfiles/bin/default.sh ~/.tmux/tmux-powerline/themes/default.sh
+cp $HOME/dotfiles/bin/default.sh $HOME/.tmux/tmux-powerline/themes/default.sh
+chmod u+x $HOME/.tmux/tmux-powerline/themes/default.sh
