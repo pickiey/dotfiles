bac334ac52b039a1eb1719a848d2fa1da2d9d764
pickiey <>
2020-11-01 17:58:53 +0900

    backup

--- slate/slate.js
+++ slate/slate.js
@@ -69,11 +69,11 @@ var launch_and_focus = function (target) {
   };
 };
 
-slate.bind('f:alt', launch_and_focus('Finder'));
-slate.bind('t:alt', launch_and_focus('iTerm'));
-slate.bind('r:alt', launch_and_focus('Reeder'));
+//slate.bind('f:alt', launch_and_focus('Finder'));
+slate.bind('h:alt', launch_and_focus('Reeder'));
+slate.bind('l:alt', launch_and_focus('Safari'));
+slate.bind('n:alt', launch_and_focus('iTerm'));
 slate.bind('m:alt', launch_and_focus('Music'));
-slate.bind('e:alt', launch_and_focus('Safari'));
 //slate.bind('e:alt', launch_and_focus('Google Chrome'));
 //slate.bind('e:alt', launch_and_focus('Chromium'));
 
