87d7d8d8a0c6e48b6737371fe37ce662c0eb90d2
pickiey <>
2021-03-24 00:03:26 +0900

    for imac

--- tmux/bin/tmuxx.sh
+++ tmux/bin/tmuxx.sh
@@ -6,20 +6,30 @@ if [ -z $TMUX ]; then
         tmux attach
     else
         terminal_width=`tput cols`
-        width_threshold=200
+        width_threshold_macbook=250
+        width_threshold_imac=500
 
-        if [ $terminal_width -gt $width_threshold ]; then
-            tmux new-session \; split-window -h \; \
-                                split-window -v \; split-window -h    \; split-window -v -d \; select-pane -L \; select-pane -L \; \
-                                split-window -v \; split-window -h -d \; split-window -v    \; clock-mode     \; select-pane -U \; \
-                                select-pane  -U \; select-pane  -R
+        if   [ $terminal_width -lt $width_threshold_macbook ]; then
+            tmux new-session    \; \
+                split-window -v \; split-window -h    \; split-window -v -d \; select-pane -L \; select-pane -U
+        elif [ $terminal_width -lt $width_threshold_imac ]; then
+            tmux new-session    \; \
+                split-window -h \; \
+                split-window -v \; split-window -h    \; split-window -v -d \; select-pane -L \; select-pane -L \; \
+                split-window -v \; split-window -h -d \; split-window -v    \; clock-mode     \; select-pane -U \; select-pane -R \; select-pane -U \; select-pane  -R
         else
-            tmux new-session \; split-window -v \; split-window -h    \; split-window -v -d \; select-pane -L \; select-pane -U
+            tmux new-session    \; \
+                split-window -h \; split-window -h    \; split-window -h    \; select-layout even-horizontal    \; \
+                split-window -v \; split-window -h    \; split-window -v -d \; select-pane -L \; select-pane -L \; \
+                split-window -v \; select-pane  -L    \; \
+                split-window -v \; select-pane  -L    \; \
+                split-window -v \; split-window -h -d \; split-window -v    \; clock-mode     \; select-pane -U \; select-pane -R \; select-pane -U \; select-pane  -L
         fi
     fi
 fi
 
 unset terminal_width
-unset width_threshold
+unset width_threshold_macbook
+unset width_threshold_imac
 
 exit
